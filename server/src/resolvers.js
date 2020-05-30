5ac86d6e1e3fa9cd4012f61579ef548f9593ee0a
Alfred Amoah <alfredasare101@gmail.com>
2020-05-30 13:29:04 +0000

    connected json-server and axios to server

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,58 +1,50 @@
 const _ = require("lodash");
+const axios = require("axios");
 
-// dummy data
-const books = [
-    { title: 'Name of the Wind', subType: 'Fantasy', genre: "FICTION", id: '1', authorId: '1' },
-    { title: 'The Final Empire', subType: 'Fantasy', genre: "NON_FICTION", id: '2', authorId: '2' },
-    { title: 'The Hero of Ages', subType: 'Fantasy', genre: "FICTION", id: '4', authorId: '2' },
-    { title: 'The Long Earth', subType: 'Sci-Fi', genre: "FICTION", id: '3', authorId: '3' },
-    { title: 'The Colour of Magic', subType: 'Fantasy', genre: "FICTION", id: '5', authorId: '3' },
-    { title: 'The Light Fantastic', subType: 'Fantasy', genre: "FICTION", id: '6', authorId: '3' },
-];
-
-const authors = [
-    {name: 'Patrick Rothfuss', age: 44, id: '1'},
-    {name: 'Brandon Sanderson', age: 42, id: '2'},
-    {name: 'Terry Pratchett', age: 66, id: '3'}
-];
 
 module.exports = {
     Query: {
         book(__, {input}, ctx) {
-            return _.find(books, {
-                id: input.id
-            });
+            return axios(`http://localhost:3004/books/${input.id}`)
+                .then(result => result.data);
+
         },
 
         books(__, {input}, ctx) {
-            return books;
+            return axios("http://localhost:3004/books")
+                .then(results => results.data);
         },
 
         author(__, {input}, ctx) {
-            return _.find(authors, {
-                id: input.id
-            });
+            return axios(`http://localhost:3004/authors/${input.id}`)
+                .then(result => result.data);
         },
 
         authors(__, {input}, ctx) {
-            return authors;
+            return axios("http://localhost:3004/authors")
+                .then(results => results.data);
         }
-
     },
 
     Book: {
         author(book, __, ctx) {
-            return _.find(authors, {
-                id: book.authorId
-            });
+            return axios("http://localhost:3004/authors")
+                .then(results => {
+                    return _.find(results.data, {
+                        id: book.authorId
+                    })
+                });
         }
     },
 
     Author: {
         books(author, __, ctx) {
-            return _.filter(books, {
-                authorId: author.id
-            });
+            return axios("http://localhost:3004/books")
+                .then(results => {
+                    return _.filter(results.data, {
+                        authorId: author.id
+                    });
+                });
         }
     }
 };
